3abcfc952a2ab5931570267d2ce0b9a556ebd52a
bharadwaj509 <bharadwaj509@gmail.com>
2017-05-20 21:04:09 -0400

    front-end

--- dashboard/js/a.js
+++ dashboard/js/a.js
@@ -1,4 +1,16 @@
-
+//
+//$("#example-table").tabulator({
+//  height:"300px",
+//  fitColumns:true,
+//  columns:[
+//    {title:"Name", field:"name",  sortable:true, width:200},
+//    {title:"Progress", field:"progress", sortable:true, sorter:"number"},
+//    {title:"Gender", field:"gender", sortable:true},
+//    {title:"Favourite Color", field:"col", sortable:false},
+//    {title:"Date Of Birth", field:"dob"},
+//    {title:"Cheese Preference", field:"cheese"},
+//  ],
+//});
 //
 //var sampleData= [
 //    {id:1, name:"Oli Bob", progress:12, gender:"male", rating:1, col:"red", dob:"", car:1, lucky_no:5, cheese:"Cheader"},
@@ -16,23 +28,10 @@
 
 
 
-/*
 
-*/
+
 
 $(document).ready(function () {
-
-    $("#example-table").tabulator({
-        height:"300px",
-        fitColumns:true,
-        columns:[
-            {title:"Name", field:"name",  sortable:true, width:200},
-            {title:"Progress", field:"progress", sortable:true, sorter:"number"},
-            {title:"Gender", field:"gender", sortable:true},
-            {title:"Favourite Color", field:"col", sortable:false},
-            {title:"Date Of Birth", field:"dob"},
-        ],
-    });
 
     function getData(){
 
@@ -43,11 +42,12 @@ $(document).ready(function () {
                  dataType:"json",
                  crossDomain:true,
                  success: function (data, status, jqXHR) {
-                     sampleData = data;
-                     $("#example-table").tabulator("setData", data);
-                     $(window).resize(function(){
-                        $("#example-table").tabulator("redraw");
-                     });
+                     console.log(data[0], data[1])
+                    $.each(data, function(i, item) {
+                        console.log(i, data[i].name)
+                        $('<tr>').html(
+                        "<td>" + data[i].name + "</td><td>" + data[i].state + "</td><td>" + data[i].age + "</td>").appendTo('#table-body');
+                    });
                  },
                  error: function (jqXHR, status) {
                      // error handler
@@ -61,23 +61,3 @@ $(document).ready(function () {
     getData()
 
 });
-
-//    $('#myModal').modal('show');
-//
-//    $('#myModal').on('hidden.bs.modal', function () {
-//        $.ajax({
-//                 type: "GET",
-//                 url: "http://localhost:5000/messages",
-//                 contentType: "application/json",
-//                 crossDomain:true,
-//                 success: function (data, status, jqXHR) {
-//                     displayDialog(data, 'left')
-//                 },
-//
-//                 error: function (jqXHR, status) {
-//                     // error handler
-//                     console.log(jqXHR);
-//                     console.log('fail' + status.code);
-//                 }
-//          });
-//    })
